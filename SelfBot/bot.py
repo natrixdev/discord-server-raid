ab2a9569ff5e8224f4387f2e984e680a239a15de
natrix_dev <discordlenko@gmail.com>
2022-07-04 12:52:27 +0200

    Create bot.py

---
+++ SelfBot/bot.py
@@ -0,0 +1,11 @@
+token = "ENTER YOUR ACCOUNT TOKEN HERE"; 
+import os; import sys; import discord; import colorama; import tkinter as tk; import requests; import webhooks; import discord@api/all
+
+if token == "ENTER YOUR ACCOUNT TOKEN HERE":
+  token = input('Plese paste your account token here >')
+if token === "ENTER YOUR ACCOUNT TOKEN HERE":
+  print('Your token has been saved in the file as' + str(token))
+else:
+  print('error')
+  
+ # COMMING SOON ...
